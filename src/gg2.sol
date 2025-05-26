13d97cafe6f425bcea61a71784aa7a34bee6db73
Mate Soos <mate.soos@argot.org>
2025-05-26 10:02:28 +0200

    new

---
+++ src/gg2.sol
@@ -0,0 +1,20 @@
+import {Test} from "forge-std/Test.sol";
+
+contract ERC20 {
+  function f() public view { }
+}
+
+contract TEST is Test{
+  address[] tokens;
+  address any = address(0x1234);
+  mapping(address => uint256) balances;
+
+  constructor() {
+    tokens.push(address(new ERC20()));
+  }
+
+  function prove_ggggKKK(address target) public {
+    balances[target] = any.balance;
+    ERC20(address(tokens[0])).f(); 
+  }
+}
