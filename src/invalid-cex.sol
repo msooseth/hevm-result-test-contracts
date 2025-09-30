5b28408d4378f627369991db3df69520896c915a
Mate Soos <mate.soos@argot.org>
2025-09-30 14:46:27 +0200

    Nope

--- src/invalid-cex.sol
+++
@@ -1,9 +0,0 @@
-contract MyContractTest is Test {
-  mapping (address => uint) balances;
-  function setUp() public {
-    balances[address(0x1234)] = 50;
-  }
-  function prove_balance(address x) public view {
-    assert(balances[x] != 40);
-  }
-}
